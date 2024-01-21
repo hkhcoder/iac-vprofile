6b4958d255dd3a8845d7ea186e51ac4ce90b9612
johaninds <115527734+johaninds@users.noreply.github.com>
2024-01-22 01:14:29 +0700

    bucket and region updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofile-gitops-johan"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
