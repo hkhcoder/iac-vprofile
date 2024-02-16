cd6b07dffb9b4a333dc40c388561984467126888
hvorostya <hvorostyaiman@gmail.com>
2024-02-16 17:31:34 +0200

    Bucket name and Cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactions21"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
