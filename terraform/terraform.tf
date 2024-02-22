b9363e7001bdeab224d552bb14cd67eceb4bf451
lutfar1996 <lutfar.rahman569@gmail.com>
2024-02-22 10:55:40 +0600

    Update the Region

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,11 +29,13 @@ terraform {
   backend "s3" {
     bucket = "vprofile-gitops-action"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
 }
 ##
 ##
+##
+
 ##
