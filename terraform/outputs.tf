78a2a645d395e2324b382163b1d466bda58a58e1
lutfar1996 <lutfar.rahman569@gmail.com>
2024-02-22 10:50:09 +0600

    Terraform Version Change

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,3 +18,7 @@ output "cluster_security_group_id" {
   description = "Security group ID for the Amazon Web Service EKS Cluster "
   value       = module.eks.cluster_security_group_id
 }
+
+
+
+###
